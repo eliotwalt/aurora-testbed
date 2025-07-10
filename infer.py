bf8177d3fa1bffacb666a00e8c0c73e358714260
eliotwalt <walt.eliot@hotmail.com>
2025-07-10 11:39:42 +0200

    inference

--- infer.py
+++ infer.py
@@ -3,6 +3,8 @@ import gc
 import datetime
 import contextlib
 import argparse
+from tqdm import tqdm
+import dataclasses
 
 import torch
 import torch.nn as nn
@@ -35,15 +37,19 @@ def make_batch(x, lat, lon):
         )
     )
     
-def compute_loss(criterion, preds, target):
-    loss = None
-    for var_kind in ["surf_vars", "atmos_vars"]:
-        for var_name in getattr(target, var_kind).keys():
-            pred_var = getattr(preds, var_kind)[var_name]
-            target_var = getattr(target, var_kind)[var_name]
-            if loss is None: loss = criterion(pred_var, target_var)
-            else: loss += criterion(pred_var, target_var)
-    return loss
+def concat_ic(batch, preds):
+    batch = dataclasses.replace(
+        preds,
+        surf_vars={
+            k: torch.cat([batch.surf_vars[k][:, 1:], v], dim=1)
+            for k, v in preds.surf_vars.items()
+        },
+        atmos_vars={
+            k: torch.cat([batch.atmos_vars[k][:, 1:], v], dim=1)
+            for k, v in preds.atmos_vars.items()
+        },
+    )   
+    return batch
 
 def custom_activation_checkpointing(model, module_names=("Perceiver3DEncoder","Swin3DTransformerBackbone","Basic3DEncoderLayer","Basic3DDecoderLayer","Perceiver3DDecoder","LinearPatchReconstruction")):
         def check(x: torch.nn.Module) -> bool:
@@ -51,11 +57,11 @@ def custom_activation_checkpointing(model, module_names=("Perceiver3DEncoder","S
             return name in module_names
         apply_activation_checkpointing(model, check_fn=check)
 
-def train(args):
+def infer(args):
     local_rank = int(os.environ["LOCAL_RANK"])
     dist.init_process_group(backend='nccl')
     device = torch.device(f'cuda:{local_rank}')
-    if local_rank == 0: print("Starting Aurora training script...")
+    if local_rank == 0: print("Starting Aurora inference script...")
     
     # Dummy dataset
     N = 5  # Number of samples
@@ -64,55 +70,49 @@ def train(args):
     lat=torch.linspace(90, -90, H)
     lon=torch.linspace(0, 360, W+1)[:-1]
     random_input = torch.randn(N, 2, M, H, W, dtype=torch.bfloat16)
-    random_target = torch.randn(N, 1, M, H, W, dtype=torch.bfloat16)
-    dataset = TensorDataset(random_input, random_target)
+    dataset = TensorDataset(random_input)
     sampler = DistributedSampler(dataset, shuffle=True)
     dataloader = DataLoader(dataset, sampler=sampler, batch_size=1, num_workers=1)
     print("Dummy dataset created with random data.")
     
     # model
-    if args.small:
-        model = AuroraSmallPretrained(bf16_mode=args.bf16, use_lora=False)
-    else:
-        model = Aurora(bf16_mode=args.bf16, use_lora=False)
+    if args.small: model = AuroraSmallPretrained(bf16_mode=args.bf16, use_lora=False)
+    else: model = Aurora(bf16_mode=args.bf16, use_lora=False)
     model.load_checkpoint(strict=False)
     custom_activation_checkpointing(model, args.checkpointing_module_names)
     model = model.to(device)
+    # full model freeze, might be unnecessary
+    model = model.eval()
+    for param in model.parameters():
+        param.requires_grad = False    
     model = DDP(model, device_ids=[local_rank])
-    print("Model loaded and wrapped in DDP.")
-    
-    # loss and optimizer
-    criterion = nn.L1Loss().to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5, lr=5e-6)
-    print("Loss and optimizer configured.")
+    model.eval()
+    print("Model loaded, frozen and wrapped in DDP.")
     
     # autocast context
     if args.autocast: autocast_context = torch.autocast(device_type='cuda', dtype=torch.bfloat16)
     else: autocast_context = contextlib.nullcontext()
     
-    # training loop
-    for epoch in range(args.num_epochs):
-        model.train()
-        for i, (inputs, targets) in enumerate(dataloader):
+    # inference loop
+    with torch.no_grad():
+        for i, (batch, ) in enumerate(dataloader):
             # to batch and move to device
-            inputs, targets = map(lambda x: make_batch(x, lat, lon)._fmap(lambda x: x.to(device, non_blocking=True)), (inputs, targets))
-            optimizer.zero_grad()
-            # fwd
-            with autocast_context:
-                outputs = model(inputs)
-                loss = compute_loss(criterion, outputs, targets)
-            # bwd 
-            loss.backward()
-            optimizer.step()
-            if local_rank == 0: print(f"Epoch [{epoch+1}/{args.num_epochs}], Step [{i+1}/{len(dataloader)}], Loss: {loss.item():.4f}")
+            batch = make_batch(batch, lat, lon)._fmap(lambda x: x.to(device, non_blocking=True))
+            # forecast loop
+            pbar = tqdm(total=args.num_steps, desc=f"Batch [{i+1}/{len(dataloader)}]", disable=(local_rank != 0))          
+            for step in range(args.num_steps):
+                with autocast_context:
+                    preds = model(batch)  
+                batch = concat_ic(batch, preds)
+                pbar.update(1)
                 
 def main(args):
-    try: train(args)
+    try: infer(args)
     except Exception as e: dist.destroy_process_group() ; raise e
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs to train for")
+    parser.add_argument("--num_steps", type=int, default=10, help="Number of forecast steps")
     parser.add_argument("--small", action="store_true", help="Use the small version of the Aurora model")
     parser.add_argument("--bf16", action="store_true", help="Use `bf16_mode`")
     parser.add_argument("--autocast", action="store_true", help="Use autocast context with bf16")
---
+++ infer_1gpu.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH --job-name=aurora-ddp
+#SBATCH --partition=gpu_h100
+#SBATCH --time=10:00
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=16
+
+source env/venv_h100/bin/activate
+
+option=$1
+
+case "$option" in
+    1)
+        # small bf16, no autocast, all checkpointing
+        srun torchrun --standalone --nnodes=1 --nproc_per_node=1 \
+            infer.py \
+            --num_steps=196 --small --bf16 \
+            --checkpointing_module_names Perceiver3DEncoder Swin3DTransformerBackbone Basic3DEncoderLayer Basic3DDecoderLayer Perceiver3DDecoder LinearPatchReconstruction
+        ;;
+    2)
+        # small no bf16, autocast, all checkpointing
+        srun torchrun --standalone --nnodes=1 --nproc_per_node=1 \
+            infer.py \
+            --num_steps=196 --small --autocast \
+            --checkpointing_module_names Perceiver3DEncoder Swin3DTransformerBackbone Basic3DEncoderLayer Basic3DDecoderLayer Perceiver3DDecoder LinearPatchReconstruction
+        ;;
+    3)
+        # small no bf16, autocast, Basic3D checkpointing
+        srun torchrun --standalone --nnodes=1 --nproc_per_node=1 \
+            infer.py \
+            --num_steps=196 --small --autocast \
+            --checkpointing_module_names Basic3DEncoderLayer Basic3DDecoderLayer
+        ;;
+    4)
+        # large bf16, no autocast, all checkpointing
+        srun torchrun --standalone --nnodes=1 --nproc_per_node=1 \
+            infer.py \
+            --num_steps=196 --bf16 \
+            --checkpointing_module_names Perceiver3DEncoder Swin3DTransformerBackbone Basic3DEncoderLayer Basic3DDecoderLayer Perceiver3DDecoder LinearPatchReconstruction
+        ;;
+    5)
+        # large no bf16, autocast, all checkpointing
+        srun torchrun --standalone --nnodes=1 --nproc_per_node=1 \
+            infer.py \
+            --num_steps=196 --autocast \
+            --checkpointing_module_names Perceiver3DEncoder Swin3DTransformerBackbone Basic3DEncoderLayer Basic3DDecoderLayer Perceiver3DDecoder LinearPatchReconstruction  
+        ;;
+    6)
+        # large no bf16, autocast, Basic3D checkpointing
+        srun torchrun --standalone --nnodes=1 --nproc_per_node=1 \
+            infer.py \
+            --num_steps=196 --autocast \
+            --checkpointing_module_names Basic3DEncoderLayer Basic3DDecoderLayer 
+        ;;
+    *)
+        echo "Usage: $0 {1|2|3|4|5|6}"
+        exit 1
+        ;;
+esac
